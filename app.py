c2a418992efe7e00e3f707b92576bc0ab77b2c67
ErikG1320 <158307983+ErikG1320@users.noreply.github.com>
2024-05-20 22:39:12 -0500

    cambios en la app

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import random 
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizer import sgd_experimental
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 
